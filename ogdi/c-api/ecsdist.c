4ebdc369b62e91cfefac73f4ebae6c6fcca6f4de
Frank Warmerdam <warmerdam@pobox.com>
2000-11-03 02:58:51 +0000

    LAS to Crown license update

--- ogdi/c-api/ecsdist.c
+++ ogdi/c-api/ecsdist.c
@@ -8,9 +8,17 @@
  *
  * General library of functions to calculate distances between objects
  *
- * Copyright (c) 1995 Logiciels et Applications Scientifiques (L.A.S.) Inc.
- * Il est strictement interdit de publier ou de devoiler le contenu de ce
- * programme sans avoir prealablement obtenu la permission de L.A.S. Inc.
+ * Copyright (C) 1995 Her Majesty the Queen in Right of Canada.
+ * Permission to use, copy, modify and distribute this software and
+ * its documentation for any purpose and without fee is hereby granted,
+ * provided that the above copyright notice appear in all copies, that
+ * both the copyright notice and this permission notice appear in
+ * supporting documentation, and that the name of Her Majesty the Queen
+ * in Right  of Canada not be used in advertising or publicity pertaining
+ * to distribution of the software without specific, written prior
+ * permission.  Her Majesty the Queen in Right of Canada makes no
+ * representations about the suitability of this software for any purpose.
+ * It is provided "as is" without express or implied warranty.
  */
 
 #include "ecs.h"
