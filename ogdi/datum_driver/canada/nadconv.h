c67a57b06b39bdda332dd65ce543a11d16a5deff
Frank Warmerdam <warmerdam@pobox.com>
2000-11-10 17:02:21 +0000

    restore LAS copyright, but use crown copyright terms (open source)

--- ogdi/datum_driver/canada/nadconv.h
+++ ogdi/datum_driver/canada/nadconv.h
@@ -31,15 +31,14 @@
  * Based on the Fortran program INTGRID.
  *
  *
- * Copyright (C) 1997 Her Majesty the Queen in Right of Canada.
+ * Copyright (C) 1997 Logiciels et Applications Scientifiques (L.A.S.) Inc
  * Permission to use, copy, modify and distribute this software and
  * its documentation for any purpose and without fee is hereby granted,
  * provided that the above copyright notice appear in all copies, that
  * both the copyright notice and this permission notice appear in
- * supporting documentation, and that the name of Her Majesty the Queen
- * in Right  of Canada not be used in advertising or publicity pertaining
- * to distribution of the software without specific, written prior
- * permission.  Her Majesty the Queen in Right of Canada makes no
+ * supporting documentation, and that the name of L.A.S. Inc not be used 
+ * in advertising or publicity pertaining to distribution of the software 
+ * without specific, written prior permission. L.A.S. Inc. makes no
  * representations about the suitability of this software for any purpose.
  * It is provided "as is" without express or implied warranty.
  *
