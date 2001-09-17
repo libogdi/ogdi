4211b8ad44d7b6718c578c545de85f660c67cb3f
Frank Warmerdam <warmerdam@pobox.com>
2001-09-17 18:33:45 +0000

    fixed up for Linux

--- mkbindist.sh
+++ mkbindist.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ $# -lt 1 ] ; then
-  echo "Usage: mkbindist.sh version [-install]"
+  echo "Usage: mkbindist.sh version"
   echo
   echo "Example: mkbindist.sh 1.1.5"
   exit
@@ -20,13 +20,16 @@ rm -rf $DIST_DIR
 mkdir $DIST_DIR
 
 mkdir $DIST_DIR/bin
-mkdir $DIST_DIR/lib
 mkdir $DIST_DIR/include
-mkdir $DIST_DIR/sample
 
+cp bin/$TARGET/* $DIST_DIR/bin
 cp bin/$TARGET/*.* $DIST_DIR/bin
-cp lib/$TARGET/ogdi* $DIST_DIR/lib
 cp ogdi/include/*.h $DIST_DIR/include
+
+if test "$TARGET" = "win32" ; then
+  mkdir $DIST_DIR/lib
+  cp lib/$TARGET/ogdi* $DIST_DIR/lib
+fi
 
 rm -f ${DIST_DIR}.tar.gz
 tar cf ${DIST_DIR}.tar ${DIST_DIR}
