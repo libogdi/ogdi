b1f8676f3c74504fa617beeef6dd6fc46f9eb2c0
Frank Warmerdam <warmerdam@pobox.com>
2001-12-14 21:22:02 +0000

    dont reproject to latlong if already latlong

--- contrib/ogdi_info/ogdi_info.c
+++ contrib/ogdi_info/ogdi_info.c
@@ -20,7 +20,10 @@
  ******************************************************************************
  *
  * $Log$
- * Revision 1.10  2001-08-17 00:37:15  warmerda
+ * Revision 1.11  2001-12-14 21:22:02  warmerda
+ * dont reproject to latlong if already latlong
+ *
+ * Revision 1.10  2001/08/17 00:37:15  warmerda
  * updated formatting of capabilities bounds
  *
  * Revision 1.9  2001/07/17 19:08:01  warmerda
@@ -232,7 +235,8 @@ static int DumpGlobalRegion( ecs_Region * region, PJ * proj_defn )
 /*	Print the corner coordinates in lat/long.			*/
 /* -------------------------------------------------------------------- */
 #ifndef _WINDOWS    
-    if( proj_defn != NULL ) {
+    if( proj_defn != NULL )
+    {
         printf( "Lat/Long Corners\n" );
         
         proj_pnt.v = region->north;
@@ -413,7 +417,9 @@ static int AccessURL( char * url, ecs_Region * region )
     printf( "Projection = `%s'\n", ECSTEXT(result) );
 
 #ifndef _WINDOWS    
-    if( !bNoProj )
+    if( !bNoProj 
+        && strstr(ECSTEXT(result),"latlong") == NULL
+        && strstr(ECSTEXT(result),"longlat") == NULL )
         proj_defn = cln_ProjInit( ECSTEXT(result) );
 #endif
 
