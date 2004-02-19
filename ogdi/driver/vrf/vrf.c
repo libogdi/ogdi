c6c47ce88e4f314827752ef71842436dc80a575e
Frank Warmerdam <warmerdam@pobox.com>
2004-02-19 06:56:43 +0000

    fixed serious bug in releaseAllLayers() with multiple layers

--- ogdi/driver/vrf/vrf.c
+++ ogdi/driver/vrf/vrf.c
@@ -17,7 +17,10 @@
  ******************************************************************************
  *
  * $Log$
- * Revision 1.14  2004-02-19 05:59:22  warmerda
+ * Revision 1.15  2004-02-19 06:56:43  warmerda
+ * fixed serious bug in releaseAllLayers() with multiple layers
+ *
+ * Revision 1.14  2004/02/19 05:59:22  warmerda
  * Removed temporary debug messages.
  *
  * Revision 1.13  2004/02/18 21:49:18  warmerda
@@ -589,10 +592,11 @@ void
 vrf_releaseAllLayers(s)
      ecs_Server *s;
 {
-  int i;
+    int i;
 
-  for (i = 0; i < s->nblayer; ++i)
-    dyn_ReleaseLayer(s,&(s->layer[i].sel));
+    /* count down since nblayer will change as we go */
+    for (i = s->nblayer-1; i >= 0; i--)
+        dyn_ReleaseLayer(s,&(s->layer[i].sel));
 }
 
 /* ----------------------------------------------------------------------
