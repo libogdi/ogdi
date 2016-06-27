4087788d0ddbb9dc9340ff7c4f74df23cfc2f75d
Even Rouault <even.rouault@spatialys.com>
2016-06-27 20:05:48 +0000

    Compilation fix against proj 4.9

--- ogdi/datum_driver/usa/dtusa.c
+++ ogdi/datum_driver/usa/dtusa.c
@@ -124,7 +124,11 @@ int dyn_nad_init(privdata, table)
     strcat(pathfile,"/");  
     strcat(pathfile,table);  
 
+#if PJ_VERSION >= 490
+    ptr->dtptr = nad_init(pj_get_default_ctx(), pathfile);
+#else
     ptr->dtptr = nad_init(pathfile);
+#endif
     if (ptr->dtptr == NULL) {
       ptr->count--;
       return FALSE;
