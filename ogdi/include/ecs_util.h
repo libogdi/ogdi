6240b95519a8ef818764cd2bfdfb1650dbc989d2
Even Rouault <even.rouault@spatialys.com>
2019-03-01 14:15:07 +0100

    Prepare for OGDI 4.0.0 release

--- ogdi/include/ecs_util.h
+++ ogdi/include/ecs_util.h
@@ -108,8 +108,8 @@
 #ifndef ECS_UTIL
 #define ECS_UTIL 1
 
-#define OGDI_VERSION     321
-#define OGDI_RELEASEDATE 20180301
+#define OGDI_VERSION     400
+#define OGDI_RELEASEDATE 20190301
 
 #include <stdio.h>
 #include <stdlib.h>
